af789a7ba1e59bb47d18401538562a76fc4ab041
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-17 11:18:29 -0500

    Initial commit

---
+++ src/main/java/com/example/PathChat/PathChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.PathChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathChatApplication.class, args);
+	}
+
+}
